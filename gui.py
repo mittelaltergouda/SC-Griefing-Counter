a7c7d6723e19f00de6c4ef0a53b9b7952afd41ec
Mittelalter Gouda <Eras308@users.noreply.github.com>
2025-04-07 21:05:44 +0200

    Version auf 0.7.9 aktualisiert

--- gui.py
+++ gui.py
@@ -20,7 +20,7 @@ from ui_constants import Colors, Fonts, WindowSettings, RefreshSettings
 import logger
 
 # Aktuelle Version der Anwendung - wird bei jedem Release aktualisiert
-APP_VERSION = "0.7.8"
+APP_VERSION = "0.7.9"
 
 # Um den Kalender zu benutzen, benötigst du das Paket tkcalendar
 # Installiere es mit: pip install tkcalendar
